3171f563e4570ae21f182a95074abaeb20f6ca6d
MADESH P B <madeshpb5@gmail.com>
2026-08-09 16:52:52 +0530

    Add AND gate tb changes

--- Day_01_Verilog_Fundamentals/and_gate_tb.v
+++ Day_01_Verilog_Fundamentals/and_gate_tb.v
@@ -8,14 +8,14 @@ module and_gate_tb() ;
 $monitor("A=%b,B=%b,Y=%b",A,B,Y) ; 
  initial
    begin 
-     A=0,B=0; 
+     A=0;B=0; 
      #10
-     A=0,B=1; 
+     A=0;B=1; 
      #10
-     A=1,B=0; 
+     A=1;B=0; 
      #10
-     A=1,B=1; 
+     A=1;B=1; 
      #50
-     $finish
+     $finish;
  end
 endmodule
