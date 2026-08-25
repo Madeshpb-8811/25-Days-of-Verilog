baaa9caa2b3ad388f3eccb3730989437156f1c75
MADESH P B <madeshpb5@gmail.com>
2026-08-25 23:57:18 +0530

    Add down_counter rtl

---
+++ Day_11_Verilog_Counters/Down_Counter/down_counter.v
@@ -0,0 +1,14 @@
+module down_counter( 
+input clk,rst,
+output reg [3:0] q 
+);
+always @(posedge clk)
+begin 
+ if(rst)
+q<=4'b1111; 
+else if(q==4'b0000)  
+q<=4'b1111; 
+else 
+q<=q-1'b1; 
+end
+endmodule
