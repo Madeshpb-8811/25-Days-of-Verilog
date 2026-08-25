ec3950415ae9e89b0a7fa631bbfdedf05980ecc4
MADESH P B <madeshpb5@gmail.com>
2026-08-26 00:09:34 +0530

    Add files via upload

---
+++ Day_11_Verilog_Counters/Johnson_Counter/johnson_counter_tb.v
@@ -0,0 +1,24 @@
+module johnson_counter_tb();    
+reg clk,rst;
+wire [3:0] q;
+johnson_counter dut(   
+.clk(clk),
+.rst(rst),
+.q(q));
+always  
+#5 clk =~clk;
+
+initial
+begin 
+$monitor("CLOCK=%b,RESET=%b,Q=%b",clk,rst,q);
+clk=0;
+rst=1;#10
+rst=0;#80
+rst=1;#10
+rst=0;#40 
+
+$finish;
+
+
+end
+endmodule
